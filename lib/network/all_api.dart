a4dce70183913f318e099385049e730bae90adcb
Ankit002252 <ankit.diff007@gmail.com>
2023-01-17 11:01:11 +0530

    -Api call add

---
+++ lib/network/all_api.dart
@@ -0,0 +1,6 @@
+// import '../model/user_model.dart';
+// import 'api_call.dart';
+//
+// Future<UserModel> getUserList({required Map<String, dynamic> request}) async {
+//   return UserModel.fromJson(await (handleResponse(await buildHttpResponse( request: request, method: HttpMethod.GET))));
+// }
